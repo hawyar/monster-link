69a4713901460774495e10b48928e67c75a3c3d1
Hawyar <hawyarxoshnaw@gmail.com>
2020-11-30 21:50:10 -0500

    remove old styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,3 @@
-import { ThemeProvider } from 'styled-components';
-import GlobalStyle from '../theme/globalStyle';
-import { Reset } from 'styled-reset';
-import { theme } from '../theme/theme';
 import { ChakraProvider } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }) {
